b48b545e3f889aeeb6486bc8b41753bfa1042569
nivetha199 <133622495+nivetha199@users.noreply.github.com>
2023-05-21 19:51:17 +0530

    day 2
    class with mai

---
+++ src/day02/program3.java
@@ -0,0 +1,16 @@
+package day02;
+
+public class program3 {
+
+	public static void main(String[] args) {
+		//main method
+		/*
+		 * Today we are seeing package
+		 * and 
+		 * class program
+		 */
+		System.out.println("good morning..!");
+		System.out.println("good evening..!");
+	}
+
+}
